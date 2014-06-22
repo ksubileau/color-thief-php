2c0d47587042950c7ba522666e6be5ae22f846c9
Kévin Subileau <github@kevinsubileau.fr>
2014-06-22 21:30:24 +0200

    Fix invalid alpha values with Imagick.

--- lib/ColorThief/Image/Adapter/ImagickImageAdapter.php
+++ lib/ColorThief/Image/Adapter/ImagickImageAdapter.php
@@ -49,13 +49,15 @@ class ImagickImageAdapter extends ImageAdapter
     {
         $pixel = $this->resource->getImagePixelColor($x, $y);
 
-        $colorArray = $pixel->getColor();
-        $color = new \stdClass();
-        $color->red = $colorArray['r'];
-        $color->green = $colorArray['g'];
-        $color->blue = $colorArray['b'];
+        // Un-normalized values don't give a full range 0-1 alpha channel
+        // So we ask for normalized values, and then we un-normalize it ourselves.
         $colorArray = $pixel->getColor(true);
-        $color->alpha = 255 - (int)($colorArray['a'] * 255);
+        $color = new \stdClass();
+        $color->red = round($colorArray['r'] * 255);
+        $color->green = round($colorArray['g'] * 255);
+        $color->blue = round($colorArray['b'] * 255);
+        $color->alpha = 127 - round($colorArray['a'] * 127);
+
         return $color;
     }
 }
