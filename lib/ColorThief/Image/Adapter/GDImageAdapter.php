81285a82576e16c3990e81ee317ac4b2c5223bd5
mreiden <mark@everytruckjob.com>
2018-10-14 20:36:07 -0500

    Remove GD webp support.  Will open separate PR.

--- lib/ColorThief/Image/Adapter/GDImageAdapter.php
+++ lib/ColorThief/Image/Adapter/GDImageAdapter.php
@@ -72,10 +72,6 @@ class GDImageAdapter extends ImageAdapter
                 $resource = @imagecreatefrompng($file);
                 break;
 
-            case IMAGETYPE_WEBP:
-                $resource = @imagecreatefromwebp($file);
-                break;
-
             default:
                 throw new \RuntimeException("Image '{$file}' is not readable or does not exists.");
                 break;
