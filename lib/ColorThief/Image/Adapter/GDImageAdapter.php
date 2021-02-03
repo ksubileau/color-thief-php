de747ea0867189563da65e121e03e74f910a42d5
Kévin Subileau <github@kevinsubileau.fr>
2021-02-03 20:28:34 +0100

    Add header comment

--- lib/ColorThief/Image/Adapter/GDImageAdapter.php
+++ lib/ColorThief/Image/Adapter/GDImageAdapter.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Color Thief PHP project.
+ *
+ * (c) Kevin Subileau
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace ColorThief\Image\Adapter;
