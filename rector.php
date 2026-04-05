bcb7b18ab9408260b30fad6c244747d8a64b7d20
Joe Workman <joe@workmanmail.com>
2026-04-05 10:29:51 -0700

    Add rector for code quality

---
+++ rector.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return RectorConfig::configure()
+	->withPaths([
+		__DIR__ . '/src',
+		__DIR__ . '/tests',
+	])
+	->withSkip([
+		__DIR__ . '/vendor',
+	])
+	->withSets([
+		// Apply PHP 8.2 level set (current project requirement)
+		LevelSetList::UP_TO_PHP_82,
+
+		// Additional useful sets
+		SetList::CODE_QUALITY,
+		SetList::DEAD_CODE,
+		SetList::TYPE_DECLARATION,
+		SetList::PRIVATIZATION,
+	])
+	->withRules([
+		ExplicitNullableParamTypeRector::class,
+	])
+	->withPhpSets(php82: true);
